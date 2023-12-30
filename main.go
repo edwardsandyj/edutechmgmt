14fd2deb8c6fd92f89598db34c2d9b7b5ec883dc
edwardsandyj <edwardsandyj@gmail.com>
2023-12-30 03:13:07 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
-	"github.com/go-redis/redis/v8"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
-var redisClient *redis.Client
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
-func init() {
-	redisClient = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	// Simulate live updates (replace with actual data retrieval logic)
+	for {
+		time.Sleep(5 * time.Second)
+
+		message := "Data from the server: " + time.Now().Format("2006-01-02 15:04:05")
+		err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
 
 func main() {
-	SetupRouter()
+	router := mux.NewRouter()
+	router.HandleFunc("/ws", handleWebSocket)
+
+	fmt.Println("Server is running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
