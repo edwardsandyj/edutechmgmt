318ee82d90533be6783cc802ba40798868eefcd5
edwardsandyj <edwardsandyj@gmail.com>
2023-12-30 03:20:49 -0600

    Update main.go

--- main.go
+++ main.go
@@ -8,11 +8,30 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
+	"github.com/go-redis/redis/v8"
+	"context"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+var (
+	upgrader    = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	redisClient *redis.Client
+)
+
+type Item struct {
+	ID   string `json:"id,omitempty"`
+	Type string `json:"type,omitempty"`
+	Data string `json:"data,omitempty"`
+}
+
+func init() {
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
 }
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
@@ -23,16 +42,66 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	// Simulate live updates (replace with actual data retrieval logic)
 	for {
-		time.Sleep(5 * time.Second)
+		// Retrieve data from Redis (replace with your actual data retrieval logic)
+		ctx := context.Background()
 
-		message := "Data from the server: " + time.Now().Format("2006-01-02 15:04:05")
-		err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+		var items []Item
+
+		deviceKeys, err := redisClient.Keys(ctx, "Device:*").Result()
 		if err != nil {
-			log.Println(err)
+			log.Println("Error retrieving device data from Redis:", err)
 			return
 		}
+
+		for _, key := range deviceKeys {
+			data, err := redisClient.HGet(ctx, key, "Location").Result()
+			if err != nil {
+				log.Println("Error getting device location:", err)
+				continue
+			}
+			items = append(items, Item{ID: key, Type: "Device", Data: data})
+		}
+
+		studentKeys, err := redisClient.Keys(ctx, "Student:*").Result()
+		if err != nil {
+			log.Println("Error retrieving student data from Redis:", err)
+			return
+		}
+
+		for _, key := range studentKeys {
+			data, err := redisClient.HGet(ctx, key, "Name").Result()
+			if err != nil {
+				log.Println("Error getting student name:", err)
+				continue
+			}
+			items = append(items, Item{ID: key, Type: "Student", Data: data})
+		}
+
+		classKeys, err := redisClient.Keys(ctx, "Class:*").Result()
+		if err != nil {
+			log.Println("Error retrieving class data from Redis:", err)
+			return
+		}
+
+		for _, key := range classKeys {
+			data, err := redisClient.HGet(ctx, key, "Teacher").Result()
+			if err != nil {
+				log.Println("Error getting class teacher:", err)
+				continue
+			}
+			items = append(items, Item{ID: key, Type: "Class", Data: data})
+		}
+
+		// Send data to the client
+		err = conn.WriteJSON(items)
+		if err != nil {
+			log.Println("Error sending message to client:", err)
+			return
+		}
+
+		// Wait for the next update
+		time.Sleep(5 * time.Second)
 	}
 }
 
